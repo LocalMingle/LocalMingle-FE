4b1eb75dc48e37032669b1ced8b499277ec5bd38
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-09 17:45:18 +0900

    [추가] 메인 페이지 관련 컴포넌트

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header: React.FC = () =>{
+  return (
+    <>
+      <div>이 곳은 헤더 입니다!</div>
+      <div>로그인 전: 로고 / 로그인</div>
+      <div>로그인 후: 로고 / 글작성 / 마이페이지 / 로그아웃</div>
+    </>
+
+  )
+}
+
+export default Header
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from '../components/common/Header/Header'
+import MainPage from '../components/MainPage/MainPage'
 
 const Main: React.FC = () => {
   return (
     <>
-    <div>헤더 - 고정 컴포넌트 불러오는 곳</div>
-    <div>Main 페이지 - 배너, 카드 담기는 곳</div>
+    <Header></Header>
+    <MainPage></MainPage>
     </>
   )
 }
