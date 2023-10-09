34ec7b6296d962b2f405ff6e954a351636eb5a84
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-09 17:17:41 +0900

    [추가] 메인 route 경로 설정

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../pages/Main";
 import Join from "../pages/Join";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Main/>}></Route>
         <Route path="/join" element={<Join />} />
       </Routes>
     </BrowserRouter>
