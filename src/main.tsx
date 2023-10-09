9ba30dafafa3b31488ec77e461ff3e593c82682f
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-09 11:33:00 +0900

    [수정] React.StrictMode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </React.StrictMode>
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
 );
