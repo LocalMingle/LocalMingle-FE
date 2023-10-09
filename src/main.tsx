ece6e92995b286d361ff95eacebb1efb37497bdf
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-09 17:15:24 +0900

    [추가] 컴포넌트, page, recoil 파일 생성
    컴포넌트 : 버튼, 헤더, 모달, 샐랙터
    페이지 : 메인
    리코일 : selectors

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import App from "./App.tsx";
---
+++ src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main: React.FC = () => {
+  return (
+    <>
+    <div>헤더 - 고정 컴포넌트 불러오는 곳</div>
+    <div>Main 페이지 - 배너, 카드 담기는 곳</div>
+    </>
+  )
+}
+
+export default Main
