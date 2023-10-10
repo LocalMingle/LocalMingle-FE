db3f6694ca06be0342c0a7dee5aa1cda8d966fa6
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-10 11:17:03 +0900

    [작업 중] 헤더 틀 작업 및 경로 지정, 로그인 여부에 따른 분기 처리

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,14 +1,60 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Header: React.FC = () =>{
+  const navigate = useNavigate();
+  
+  // 로그인 여부
+  const isLogin = false;
+
+  // (로고)메인 페이지로 이동 
+  const goToMain = () => {
+    navigate('/');
+  }
+
+  // 게시글 작성 페이지로 이동
+  const goToPost = () => {
+    navigate('/post');
+  }
+
+  // 마이 페이지로 이동
+  const goToMyPage = () => {
+    navigate('/mypage');
+  }
+
+  // 로그인 페이지로 이동
+  const login = () => {
+    navigate('/login');
+  }
+
+  // 로그아웃 : 토큰 삭제
+  const logout = () => {
+    alert('로그아웃 버튼을 눌렀습니다');
+  }
+
   return (
     <>
-      <div>이 곳은 헤더 입니다!</div>
-      <div>로그인 전: 로고 / 로그인</div>
-      <div>로그인 후: 로고 / 글작성 / 마이페이지 / 로그아웃</div>
+    {isLogin === false ? (
+      /* 로그인 전 */
+      <div>
+          <span onClick={goToMain}>로고</span>
+          {/* 231010 JSY : 이미지 로고 받으면 아래 코드로 교체 예정 */}
+          {/* <img src="" alt="logo" onClick={goToMain}>로고</img> */}
+        <button onClick={login}>로그인</button>
+      </div>
+    ) : (
+      /* 로그인 후 */
+      <div>
+          <span onClick={goToMain}>로고</span>
+          {/* 231010 JSY : 이미지 로고 받으면 아래 코드로 교체 예정 */}
+          {/* <img src="" alt="logo" onClick={goToMain}>로고</img> */}
+        <button onClick={goToPost}>게시글 작성</button>
+        <button onClick={goToMyPage}>마이 페이지</button>
+        <button onClick={logout}>로그아웃</button>
+      </div>
+    )}
     </>
-
   )
-}
+};
 
 export default Header
