0cfaf5319399e0b387eab2375848b545bd524c2d
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-10 23:42:54 +0900

    [수정] index.js -> ts로 수정, Banner 컴포넌트 추가

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Banner from '../common/ Banner/Banner'
 
 const MainPage: React.FC = () => {
   return (
     <>
-      <div>Main 페이지 - 배너, 카드 담기는 곳... 좀 더 자세하게 컴포넌트 나눌 필요 있음</div>
+      <Banner></Banner>
+      <div>Main 페이지 입니다</div>
     </>
   )
 }
