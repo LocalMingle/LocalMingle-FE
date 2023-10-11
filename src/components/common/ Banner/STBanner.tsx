8c83f750e6e71a15ed884f114f2507a707a8c257
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-11 14:49:18 +0900

    [완료] 배너 컴포넌트 디자인

--- src/components/common/ Banner/STBanner.tsx
+++ src/components/common/ Banner/STBanner.tsx
@@ -4,4 +4,24 @@ export const BannerSection = styled.div`
   width: 100%;
   height: 200px;
   background-color:#E6DCF0;
+  border-radius: 10px;
+`
+
+export const BannerContent = styled.div`
+  padding-left: 22px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+
+  & h3 {
+    font-size: 20px;
+    font-weight: 600;
+    padding-bottom:14px;
+  }
+
+  & p {
+    font-size: 15px;
+    font-weight: 400;
+  }
 `
