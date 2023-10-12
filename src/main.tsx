c44a0e7aabc87e0c763ba36c93a8bf2840658a2e
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-12 22:41:41 +0900

    [추가] react-query 환경 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App.tsx";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <RecoilRoot>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </RecoilRoot>
 );
