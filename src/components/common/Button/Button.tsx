c82defc8540c92d2134458d897110f759fd3bd35
ted0729 <tutu729@naver.com>
2023-10-13 23:53:14 +0900

    [추가]회원가입,로그인 버튼

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
-const Button: React.FC = () => {
-  return (
-    <></>
-  )
+import React from "react";
+import * as St from "./STButton";
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick: () => Promise<void>;
 }
+
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+  return <St.Button onClick={onClick}>{children}</St.Button>;
+};
 
 export default Button;
