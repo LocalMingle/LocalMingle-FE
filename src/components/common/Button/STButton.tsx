12db7dd87a78d3068e2c2a67b02a5e1c3dcf5c4a
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-17 21:30:34 +0900

    [작업중] 버튼 props 설정

--- src/components/common/Button/STButton.tsx
+++ src/components/common/Button/STButton.tsx
@@ -1,4 +1,8 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+interface ButtonProps {
+  btnwidth?: string;
+}
 
 export const ButtonWrap = styled.div`
   border-radius: 23px;
@@ -7,20 +11,39 @@ export const ButtonWrap = styled.div`
   padding: 3px;
 `;
 
-export const Button = styled.button`
+export const Button = styled.button<ButtonProps>`
+  box-sizing: border-box;
   border-radius: 20px;
   border: 1px solid #adadad;
   background: #edf895;
   color: #646464;
   text-align: center;
-  width: 100px;
   height: 45px;
   padding: 8px;
   font-size: 16px;
   font-weight: 600;
   cursor: pointer;
+  /* width: 120px; */
 
   &:focus {
     outline: none;
   }
+
+  ${(props) => 
+    props.btnwidth === "large" &&
+    css`
+      width: 150px;
+    `}
+
+${(props) => 
+    props.btnwidth === "medium" &&
+    css`
+      width: 100px;
+    `}
+
+${(props) => 
+    props.btnwidth === "small" &&
+    css`
+      width: 80px;
+    `}
 `;
