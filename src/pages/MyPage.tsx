fb62ad550fd22d60856b6eb0128c8102783f4d55
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-18 02:19:12 +0900

    [수정] 마이페이지 이동시 메인화면 진입 불가로 임시 헤더 추가

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import MyPageTabs from "../components/Mypage/MypageTabs/MypageTabs";
+import { Header } from "../components/common/Header";
 
 const MyPage: React.FC = () => {
   return (
     <>
+    {/* 231018 JSY 이동 불편으로 임시로 header 컴포넌트 불러옴 */}
+      <Header></Header>
       <MyPageTabs />
       <Outlet />
     </>
