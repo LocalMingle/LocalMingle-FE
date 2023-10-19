d4fa088c641fedcaf3c3e9b695189906b24dc43c
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-19 21:41:32 +0900

    [작업중] 게시글 작성 기능 구현 중

--- src/components/PostPage/WritePost/WritePost.tsx
+++ src/components/PostPage/WritePost/WritePost.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as St from './STWritePost'
 import { useNavigate } from 'react-router-dom';
-import { useQuery } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import axios, {AxiosInstance} from 'axios';
 import { Selector } from '../../common/Selector'
 import { Button } from '../../common/Button';
 
 const STWritePost: React.FC = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const accessToken = localStorage.getItem('accessToken');
 
   // AxiosInstance & API 설정
@@ -23,20 +24,36 @@ const STWritePost: React.FC = () => {
     // locationApi :  () => customAxios.get('data/toss'),       // 위치 인증 여부
   }
 
+  // 게시글 작성 interface (console.log 기준)
+  interface WritePostData {
+    data : {
+    "eventName": string,
+    "maxSize": number,
+    "eventDate": string,
+    "signupStartDate": string,
+    "signupEndDate": string,
+    "eventLocation": string,
+    "content": string,
+    "category": string,
+    "isDeleted": boolean,
+    "isVerified": string,
+    "eventImg": string | null,
+    }
+  }
+
   // 게시글 작성 - DB 연동
-  // const { data: WritePostData } = useQuery<string>(
-  //   'WritePostData',
-  //   async () => {
-  //     const response = await mainAPI.WritePostApi()
-  //     .then(response => {
-  //       return response.data;
-  //     }).catch(error=>{
-  //         console.log('게시글 작성 실패', error);
-  //         throw error;
-  //     })
-  //     return response;
-  //   }
-  // );
+  const writePostMutation = useMutation(
+    async (postData: WritePostData) => {
+      try {
+        const response = await customAxios.post('events', postData);
+        console.log('게시글 작성 성공', response);
+        return response.data;
+      } catch (error) {
+        console.error('게시글 작성 실패', error);
+        throw error;
+      }
+    }
+  );
 
   // 게시글 취소
   const postCancel = () => {
@@ -44,9 +61,45 @@ const STWritePost: React.FC = () => {
   }
 
   // 게시글 등록
-  const postAdd = () => {
-    alert('게시글 등록 완료!');
+  const postAdd = async () => {
+    try {
+      const postData: WritePostData = {
+        data : {
+          eventName,
+          maxSize,
+          eventDate,
+          signupStartDate,
+          signupEndDate,
+          eventLocation,
+          content,
+          category,
+          isDeleted,
+          isVerified,
+          eventImg,
+        },
+      };
+      await writePostMutation.mutateAsync(postData);
+    } catch (error) {
+      console.log('게시글 작성 실패', error);
+    }
   }
+
+  const [eventName, setEventName] = useState('');
+  const [eventDate, setEventDate] = useState('');
+  const [signupStartDate, setSignupStartDate] = useState('');
+  const [signupEndDate, setSignupEndDate] = useState('');
+  const [eventLocation, setEventLocation] = useState('');
+  const [maxSize, setMaxSize] = useState(0);
+  const [content, setContent] = useState('');
+
+  const [category, setCategory] = useState('test');
+  const [isDeleted, setIsDeleted] = useState(false);
+  const [isVerified, setIsVerified] = useState('test');
+  const [eventImg, setEventImg] = useState(null);
+
+  // 사용하지 않는 변수임을 명시적으로 알리기
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const unusedVariables = { setCategory, setIsDeleted, setIsVerified, setEventImg };
 
   return (
     <>
@@ -56,30 +109,31 @@ const STWritePost: React.FC = () => {
           <Selector></Selector>
         </St.SelectorWrap>
         <St.TitleWrap>
-          <input type="text" placeholder='제목을 입력하세요'/>
+          <input type="text" placeholder='제목을 입력하세요' value={eventName} onChange={(e)=>{setEventName(e.target.value)}}/>
         </St.TitleWrap>
         <St.InputWrap>
           <div>
             <p>모임일시</p>
-            <input type="date"/>
+            <input type="date" value={eventDate} onChange={(e)=>{setEventDate(e.target.value)}}/>
           </div>
           <div>
             <p>참가신청 기간</p>
-            <input type="date"/>&nbsp;~&nbsp;
-            <input type="date"/>
+            <input type="date" value={signupStartDate} onChange={(e)=>{setSignupStartDate(e.target.value)}}/>
+            &nbsp;~&nbsp;
+            <input type="date" value={signupEndDate} onChange={(e)=>{setSignupEndDate(e.target.value)}}/>
           </div>
           <div>
             <p>모임주소</p>
-            <input type="text" placeholder='ex. 서울시 마포구'/>
+            <input type="text" placeholder='ex. 서울시 마포구' value={eventLocation} onChange={(e)=>{setEventLocation(e.target.value)}}/>
           </div>
           <div>
             <p>모임인원</p>
-            <input type="number" placeholder='ex. 10'/>
+            <input type="number" placeholder='ex. 10' value={maxSize} onChange={(e)=>{setMaxSize(parseInt(e.target.value))}}/>
             <span>명</span>
           </div>
         </St.InputWrap>
         <St.ContentsWrap>
-          <textarea placeholder='내용을 입력하세요'/>
+          <textarea placeholder='내용을 입력하세요' value={content} onChange={(e)=>{setContent(e.target.value)}}/>
         </St.ContentsWrap>
         <St.ButtonWrap>
           <Button onClick={postCancel}>취소</Button>
