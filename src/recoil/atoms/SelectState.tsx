4a4e9375750399aeff54f5e6501d831638db2c1e
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-23 16:01:16 +0900

    [수정] 샐랙터 전역 상태관리 추가

--- src/recoil/atoms/SelectState.tsx
+++ src/recoil/atoms/SelectState.tsx
@@ -1,19 +1,25 @@
-import { atom, useRecoilState } from "recoil";
+import { atom } from "recoil";
+
+// 위치인증 여부 샐랙터
+export const locationState = atom<string[]>({
+  key: 'locationState',
+  default: [],
+});
 
 // 시/도 옵션 샐랙터
 export const sidoOptionsState = atom<string[]>({
   key: 'sidoOptionsState',
   default: [],
 });
-export const useSidoOptions = () => {
-  return useRecoilState(sidoOptionsState);
-};
+
+// 구/군 옵션 샐랙터
+export const gugunOptionsState = atom<string[]>({
+  key: 'gugunOptionsState',
+  default: [],
+});
 
 // 카테고리 옵션 샐랙터
 export const categoryOptionsState = atom<string[]>({
   key: 'categoryOptionsState',
   default: [],
 });
-export const useCategoryOptions = () => {
-  return useRecoilState(categoryOptionsState);
-};
