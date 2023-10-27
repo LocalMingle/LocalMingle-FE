d50d84066b74d827c50394184638d70d45f3d1b2
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-27 20:21:17 +0900

    [수정] 불필요한 파일 제거 및 css 연결

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
-import "./index.css";
+import "./app.css";
 import "./util/Locales/i18n"; // 번역 설정을 앱 전체에 전용하는 곳
 
 const queryClient = new QueryClient();
