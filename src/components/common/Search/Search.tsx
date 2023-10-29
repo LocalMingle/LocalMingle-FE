36a61acbbbe649ac5c3c4da0743d8d18ec2d20b5
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-10-29 22:48:20 +0900

    [추가] 검색 시 빈란이면 토스트로 에러 메세지 띄우기

--- src/components/common/Search/Search.tsx
+++ src/components/common/Search/Search.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import * as St from "./STSearch";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { useLanguage } from "../../../util/Locales/useLanguage";
+import toast from "react-hot-toast";
 
 const Search: React.FC = () => {
   const { t } = useLanguage();
+  const [keyword, setKeyword] = useState<string>("");
+
+  const searchCards = () => {
+    // 빈칸 입력시 유효성 검사
+    if (keyword.length == 0 || keyword == null || keyword == undefined || keyword == "") {
+      toast.error(t("검색어를 입력해주세요!"));
+      return;
+    }
+  }
 
   return (
     <St.SearchBar>
       <div>
         <St.SearchInput
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
           placeholder={t("제목 및 글 내용을 검색해 보세요.")}
         ></St.SearchInput>
-        <p>
+        <p onClick={searchCards}>
           <FontAwesomeIcon
             icon={faMagnifyingGlass}
             style={{ color: "#646464" }}
