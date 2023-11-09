b85473e98a22563b854698d4f898b584f81ae210
ted0729 <tutu729@naver.com>
2023-11-09 16:28:53 +0900

    [수정]실시간 채팅 관련

--- src/components/Room/ChatTypes.ts
+++ src/components/Room/ChatTypes.ts
@@ -36,3 +36,22 @@ export type User = {
   nickname: string;
   profileImg: string;
 };
+
+export type ChatHistory = {
+  roomId: number;
+  chat: string;
+  userList: {
+    userId: number;
+    nickname: string;
+    profileImg: string;
+  }[];
+};
+
+export type ClientMessageData = {
+  message: string;
+  nickname: string;
+  profileImg: string;
+  time: string; // 서버 측 타임스탬프가 string 타입인지 확인 필요
+  roomId: number;
+  userId: number;
+};
